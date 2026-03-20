066de409b3cd295f966f875d9cdcfaaa2ec3ddf8
eswar7401 <106008377+eswar7401@users.noreply.github.com>
2026-03-20 17:17:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,66 +1,21 @@
 fetch("score.json")
-
 .then(response => response.json())
-
 .then(data => {
-
-const board = document.getElementById("scoreboard");
 const hall = document.getElementById("halloffame");
 
-/* SCOREBOARD */
-
-if(board){
-
-data.sort((a,b)=> b.POINTS - a.POINTS);
-
-data.forEach((team,index)=>{
-
-let card=document.createElement("div");
-
-card.className="card";
-
-card.innerHTML=`
-
-<h3>#${index+1} ${team.PARTICIPANTS}</h3>
-
-<p>${team.POINTS} pts</p>
-
-`;
-
-board.appendChild(card);
-
-});
-
-}
-
 /* HALL OF FAME */
-
 if(hall){
-
 data.sort((a,b)=> b.POINTS - a.POINTS);
-
 let top3=data.slice(0,3);
-
 top3.forEach((team,index)=>{
-
 let div=document.createElement("div");
-
 div.className="winner";
-
 div.innerHTML=`
-
 <h2>#${index+1}</h2>
-
 <h3>${team.PARTICIPANTS}</h3>
-
 <p>${team.POINTS} pts</p>
-
 `;
-
 hall.appendChild(div);
-
 });
-
 }
-
 });
